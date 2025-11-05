6412b54e25337f909eb64bba85a5fed911646e51
“Josu” <josu.ortega.52976@ikasle.egibide.org>
2025-11-05 12:14:00 +0100

    primer commit josu

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.JOptionPane;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    static int saldo_inicial = 0;
+
+    public static void main(String[] args) {
+    }
+    public static void Imposicion(){
+        String imposiciones = JOptionPane.showInputDialog(null, "Ingrese de cuanto a sido la imposicion");
+        String fecha_imposicion = JOptionPane.showInputDialog("Ingrese que dia a sido la imposicion(dd/mm/yyyy)");
+        LocalDate fecha_imp = LocalDate.parse(fecha_imposicion, dtf);
+
+
+        int imposicion = Integer.parseInt(imposiciones);
+        saldo_inicial = saldo_inicial + imposicion;
+        JOptionPane.showMessageDialog(null, imposicion + " imposicion "+ fecha_imp);
     }
 }
